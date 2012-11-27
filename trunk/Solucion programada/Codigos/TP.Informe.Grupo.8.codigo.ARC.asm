216be3eb50d9dacc7cbe615ce8f6f899da4437e8
federicomrossi@gmail.com <federicomrossi@gmail.com@b0e9351e-f111-aa17-5f96-e9940d154bab>
2012-11-27 08:11:02 +0000

    Versión RC del código en Assembly ARC

--- trunk/Solucion programada/Codigos/TP.Informe.Grupo.8.codigo.ARC.asm
+++ trunk/Solucion programada/Codigos/TP.Informe.Grupo.8.codigo.ARC.asm
@@ -26,6 +26,8 @@
 ! 
 !	%r1 - Direccion base de la region I/O de la memoria
 !	%r2 - Registro auxiliar para armado de estados a activar
+!	%r3 - Registro auxiliar para memorizacion de botones activos
+!	%r4 - Registro auxiliar para verificacion de botones activos
 !	%r5 - Contador de ciclos para el timer T1
 !	%r6 - Contador de ciclos para el timer T2
 !	%r7 - Contador de ciclos para el timer T3
@@ -38,45 +40,44 @@ IO		.equ	0x20			! 0xD6000020 Posicion de memoria donde se encuentran
 						! mapeadas las luces y pulsadores
 		
 		sethi	BASE_IO, %r1		! %r1 <-- Direccion base de la region I/O de memoria
-
-secuenciaA:	and	%r1, %r0, %r2		! %r2 <-- 0x0 - Estado de luces LA1
-		st	%r2, [%r1 + IO]	! 0xD6000020 <-- %r2 
+default:	and	%r2, 0x0, %r2		! %r2 <-- 0x0
+		st	%r2, [%r1 + IO]	! 0xD6000020 <-- %r2 (reset de los bits de la 
+						! posicion de memoria 0xD6000020)
+secuenciaA:	call	T1			! Delay de 1 segundo
+		ld 	[%r1 + IO], %r3	! %r3 <-- Valor de I/O mapeado en 0xD6000020
+		and	%r3, 0x3, %r2		! Guardamos en %r2 los dos primeros bits de %r3
+						! Estado de luces LA1 (todas las luces apagadas)
+		st	%r2, [%r1 + IO]	! 0xD6000020 <-- %r2
 		call	T1			! Delay de 1 segundo
-		and	%r2, %r0, %r2		! %r2 <-- 0x0
 		sethi	LA2, %r2		! %r2 <-- LA2 en 22 bits mas significativos
-		st	%r2, [%r1 + IO]	! 0xD6000020 <-- %r2 
-		call	T1			! Delay de 1 segundo
+		ld 	[%r1 + IO], %r3	! %r3 <-- Valor de I/O mapeado en 0xD6000020
+		and	%r3, 0x3, %r3		! Guardamos en %r3 los dos primeros bits de 0xD6000020
+		add	%r2, %r3, %r2		! %r2 <-- %r2 + %r3
+		st	%r2, [%r1 + IO]	! 0xD6000020 <-- %r2
 		ba	secuenciaA		! Reiniciamos secuencia A
 
-secuenciaB:	and	%r2, %r0, %r2		! %r2 <-- 0x0
-		sethi	LB1, %r2		! %r2 <-- LB1 en 22 bits mas significativos
+secuenciaB:	sethi	LB1, %r2		! %r2 <-- LB1 en 22 bits mas significativos
 		st	%r2, [%r1 + IO]	! 0xD6000020 <-- %r2 
 		call	T5			! Delay de 5 segundos
-		and	%r2, %r0, %r2		! %r2 <-- 0x0
 		sethi	LB2, %r2		! %r2 <-- LB2 en 22 bits mas significativos
 		st	%r2, [%r1 + IO]	! 0xD6000020 <-- %r2 
 		call	T30			! Delay de 30 segundos
-		and	%r2, %r0, %r2		! %r2 <-- 0x0
 		sethi	LB3, %r2		! %r2 <-- LB3 en 22 bits mas significativos
 		st	%r2, [%r1 + IO]	! 0xD6000020 <-- %r2 
 		call	T5			! Delay de 5 segundos
-		and	%r2, %r0, %r2		! %r2 <-- 0x0
 		sethi	LB4, %r2		! %r2 <-- LB4 en 22 bits mas significativos
 		st	%r2, [%r1 + IO]	! 0xD6000020 <-- %r2 
 		call	T5			! Delay de 5 segundos
-		and	%r2, %r0, %r2		! %r2 <-- 0x0
 		sethi	LB5, %r2		! %r2 <-- LB5 en 22 bits mas significativos
 		st	%r2, [%r1 + IO]	! 0xD6000020 <-- %r2 
 		call	T30			! Delay de 30 segundos
-		and	%r2, %r0, %r2		! %r2 <-- 0x0
 		sethi	LB6, %r2		! %r2 <-- LB6 en 22 bits mas significativos
 		st	%r2, [%r1 + IO]	! 0xD6000020 <-- %r2 
 		call	T5			! Delay de 5 segundos
 		ba	secuenciaA		! Retornamos a la secuencia A
 			
-secuenciaC:	and	%r2, %r0, %r2		! %r2 <-- 0x0
-		sethi	LC1, %r2		! %r2 <-- LA2 en 22 bits mas significativos
-		add	%r2, BB, %r2		! %r2 <-- %r2 + BB (mantenemos activo el bit de BB)
+secuenciaC:	sethi	LC1, %r2		! %r2 <-- LA2 en 22 bits mas significativos
+		add	%r2, BB, %r2		! %r2 <-- %r2 + BB (mantenemos activo el boton BB)
 		st	%r2, [%r1 + IO]	! 0xD6000020 <-- %r2 
 chequeoBB:	call	controlBB_d		! Verificacion de BB desactivado
 		ba	chequeoBB		! Bucle para permanecer en la secuencia C		
@@ -85,24 +86,21 @@ chequeoBB:	call	controlBB_d		! Verificacion de BB desactivado
 
 
 ! Rutina de verificacion de pulsador BB activado
-
-controlBB_a:	ld 	[%r1 + IO], %r2	! %r2 <-- Valor de I/O mapeado en 0xD6000020
-		andcc 	%r2, BB, %r0 		! Verifica si esta encendido el boton de bomberos
-		be 	secuenciaC		! Salto a la secuencia C
+controlBB_a:	ld 	[%r1 + IO], %r4	! %r4 <-- Valor de I/O mapeado en 0xD6000020
+		andcc 	%r4, BB, %r0 		! Verifica si esta encendido el boton de bomberos
+		bne 	secuenciaC		! Salto a la secuencia C
 		jmpl	%r15+4, %r0		! Retornamos a la rutina invocante
-
 
 ! Rutina de verificacion de pulsador BB desactivado
-controlBB_d:	ld 	[%r1 + IO], %r2	! %r2 <-- Valor de I/O mapeado en 0xD6000020
-		andcc 	%r2, BB, %r0 		! Verifica si esta encendido el boton de bomberos
-		be 	secuenciaA		! Salto a la secuencia A
+controlBB_d:	ld 	[%r1 + IO], %r4	! %r4 <-- Valor de I/O mapeado en 0xD6000020
+		andcc 	%r4, BB, %r0 		! Verifica si esta encendido el boton de bomberos
+		be 	default		! Salto a la secuencia A
 		jmpl	%r15+4, %r0		! Retornamos a la rutina invocante
 
-
 ! Rutina de verificacion de pulsador BP activado
-controlBP_a:	ld 	[%r1 + IO], %r2	! %r2 <-- Valor de I/O mapeado en 0xD6000020
-		andcc 	%r1, BP, %r0 		! Verifica si esta encendido el boton de peaton
-		be 	secuenciaB		! Salto a la secuencia B
+controlBP_a:	ld 	[%r1 + IO], %r4	! %r4 <-- Valor de I/O mapeado en 0xD6000020
+		andcc 	%r4, BP, %r0 		! Verifica si esta encendido el boton de peaton
+		bne 	secuenciaB		! Salto a la secuencia B
 		jmpl	%r15+4, %r0		! Retornamos a la rutina invocante
 
 
@@ -114,26 +112,29 @@ controlBP_a:	ld 	[%r1 + IO], %r2	! %r2 <-- Valor de I/O mapeado en 0xD6000020
 !	T5  - Timer de 5 segundos
 !	T30 - Timer de 30 segundos
 
-T1:		ld	[T1_ciclos], %r5	! %r5 <-- T1_ciclos
-loop1:		addcc	%r5, -1, %r5		! Decrementamos cantidad restante de ciclos
-		be	T_done			! Finaliza cuando no quedan ciclos por ejecutar
-		call	controlBB_a		! Realizamos control del pulsador BB
+T1:		add	%r15, %r0, %r16	! %r16 <-- %r15
+		ld	[T1_ciclos], %r5	! %r5 <-- T1_ciclos
+T1_loop:	call	controlBB_a		! Realizamos control del pulsador BB
 		call	controlBP_a		! Realizamos control del pulsador BP
-		ba	loop1			! Repetir bucle
+		addcc	%r5, -1, %r5		! Decrementamos cantidad restante de ciclos
+		be	T_done			! Finaliza cuando no quedan ciclos por ejecutar
+		ba	T1_loop		! Repetir bucle
 	
-T5:		ld	[T5_ciclos], %r5	! %r5 <-- T5_ciclos
-loop2:		addcc	%r5, -1, %r5		! Decrementamos cantidad restante de ciclos
+T5:		add	%r15, %r0, %r16	! %r16 <-- %r15
+		ld	[T5_ciclos], %r5	! %r5 <-- T5_ciclos
+T5_loop:	addcc	%r5, -1, %r5		! Decrementamos cantidad restante de ciclos
 		be	T_done			! Finaliza cuando no quedan ciclos por ejecutar
 		call	controlBB_a		! Realizamos control del pulsador BB
-		ba	loop2			! Repetir bucle
+		ba	T5_loop		! Repetir bucle
 
-T30:		ld	[T30_ciclos], %r5	! %r5 <-- T30_ciclos
-loop3:		addcc	%r5, -1, %r5		! Decrementamos cantidad restante de ciclos
+T30:		add	%r15, %r0, %r16	! %r16 <-- %r15
+		ld	[T30_ciclos], %r5	! %r5 <-- T30_ciclos
+T30_loop:	addcc	%r5, -1, %r5		! Decrementamos cantidad restante de ciclos
 		be	T_done			! Finaliza cuando no quedan ciclos por ejecutar
 		call	controlBB_a		! Realizamos control del pulsador BB
-		ba	loop3			! Repetir bucle
+		ba	T30_loop		! Repetir bucle
 
-T_done:  	jmpl	%r15+4, %r0		! Retorno a la rutina invocante
+T_done:  	jmpl	%r16+4, %r0		! Retorno a la rutina invocante
 
 ! FIN RUTINA TIMER
 
@@ -161,7 +162,7 @@ LC1		.equ	0x18180		! 0x06060000 - R1, A1, R2 y A2 encendidas
 
 ! Timers
 T1_ciclos:	1				! Cantidad de ciclos a ejecutar en T1
-T5_ciclos:	12				! Cantidad de ciclos a ejecutar en T5
-T30_ciclos:	100				! Cantidad de ciclos a ejecutar en T30
+T5_ciclos:	10				! Cantidad de ciclos a ejecutar en T5
+T30_ciclos:	40				! Cantidad de ciclos a ejecutar en T30
 
 		.end
